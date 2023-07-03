6eae2091f8b0f5ac2268faad358eb108675f8f56
Vahid Taheri <v.taheri@aranumadc.local>
2023-07-03 11:29:40 +0330

    Redesign SecondActivity

--- app/src/main/java/com/aranuma/smarthse/SecondActivity.kt
+++ app/src/main/java/com/aranuma/smarthse/SecondActivity.kt
@@ -1,8 +1,8 @@
 package com.aranuma.smarthse
 
+import android.annotation.SuppressLint
 import android.content.ContentValues.TAG
 import android.content.Intent
-import android.nfc.Tag
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -18,12 +18,14 @@ import java.util.Calendar
 class SecondActivity : AppCompatActivity() {
 
     private var deviceAddress: String? = null
+    private var transparentDataValue = false
 
+    private lateinit var versionTextView:TextView
     private lateinit var batteryTextView:TextView
     private lateinit var disconnectBtn:Button
     private lateinit var syncTimeBtn:Button
     private lateinit var getTemperatureBtn:Button
-    private lateinit var setUserBtn:Button
+    private lateinit var setTransparentData:Button
     private lateinit var sdk:SDK
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,12 +33,12 @@ class SecondActivity : AppCompatActivity() {
 
         deviceAddress = intent.getStringExtra("deviceAddress")
 
+        versionTextView = findViewById(R.id.version_textview)
         batteryTextView = findViewById(R.id.battery_percent_textview)
         disconnectBtn = findViewById(R.id.disconnect_bt_device_btn)
         syncTimeBtn = findViewById(R.id.sync_data_time_btn)
         getTemperatureBtn = findViewById(R.id.get_temperature)
-        setUserBtn = findViewById(R.id.set_user)
-
+        setTransparentData = findViewById(R.id.set_transparent_btn)
 
         sdk = SDK(applicationContext,1,callBack)
 
@@ -51,20 +53,15 @@ class SecondActivity : AppCompatActivity() {
             syncTimeBtn.text= getText(R.string.date_time_synced)
             syncTimeBtn.isEnabled=false
             syncTimeBtn.setBackgroundColor(resources.getColor(R.color.UFOGreen100))
-
-            batteryTextView.text = sdk.battery.getString("result")
         }
 
         getTemperatureBtn.setOnClickListener{
             sdk.write(ParserHelper.getTemperatureDataValue());
         }
 
-        setUserBtn.setOnClickListener{
-            try {
-                sdk.write("4D53473A4172610D0A".toByteArray(Charsets.US_ASCII))
-            } catch (e:IllegalAccessError) {
-                Log.d(TAG, e.toString())
-            }
+        setTransparentData.setOnClickListener{
+            transparentDataValue = !transparentDataValue
+            sdk.setTransparentData(!transparentDataValue)
         }
     }
     private val callBack: SDKCallBack = object : SDKCallBack() {
@@ -80,6 +77,7 @@ class SecondActivity : AppCompatActivity() {
             Log.e(TAG, "Connection status changed")
             if (state == STATE_CONNECT_SUCCESS) {
                 sdk.openDescriptor() // After connecting successfully, you must call this otherwise you will not be able to receive data.。
+
             } else if (state == STATE_DISCONNECT) {
                 startActivity(intent)
             } else if (state == STATE_CONNECT_FAIL) {
@@ -87,11 +85,14 @@ class SecondActivity : AppCompatActivity() {
             }
         }
 
+        @SuppressLint("SetTextI18n")
         override fun onDescriptorWrite(status: Int) {
             if (status == STATUS_SUCCESS) {
                 Log.e(TAG, "Successful connection")
                 runOnUiThread {
                     Toast.makeText(this@SecondActivity, "Successful connection", Toast.LENGTH_LONG).show()
+                    versionTextView.text = sdk.version.getString("result")
+                    batteryTextView.text = "Battery: ${sdk.battery.getString("result")}%"
                 }
             }
         }
@@ -106,12 +107,5 @@ class SecondActivity : AppCompatActivity() {
                 ).show()
             }
         }
-    }
-    fun String.decodeHex(): ByteArray {
-        check(length % 2 == 0) { "Must have an even length" }
-
-        return chunked(2)
-            .map { it.toInt(16).toByte() }
-            .toByteArray()
     }
 }
